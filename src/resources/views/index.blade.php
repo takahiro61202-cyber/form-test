7b1c95c88f1e46a7f97c7267ba572df0773631c0
taro <takahiro61202@gmail.com>
2026-02-28 19:31:03 +0900

    first commit

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
+@section('content')
+<div class="fashion__content">
+    <form class="create-form" action="" method="post">
+        @csrf
+        <div class="title">
+            FashionablyLate
+        </div>
+        <div class="create-form__group">
+            <div class="create-form__title">
+                <span class="create-form__label--item">
+                    お名前
+                </span>
+                <span class="create-form__label--required">
+                    ※
+                </span>
+            </div>
+            <div class="create-form__content">
+                <input type="text" name="name">
+            </div>
+        </div>
+        <div class="create-form__group">
+            <div class="create-form__tittle">
+                <span class="create-form__label--item">
+                    性別
+                </span>
+                <span class="create-form__label--required">
+                    ※
+                </span>
+            </div>
+            <div class="create-form__content">
+                <input type="radio" name="gender" value="male">
+                <span class="create-form__label--item">
+                    男性
+                </span>
+                <input type="radio" name="gender" value="female">
+                <span class="create-form__label--item">
+                    女性
+                </span>
+                <input type="radio" name="name">
+                <span class="create-form__label--item">
+                    その他
+                </span>
+            </div>
+            <div class="create-form__title">
+                <span class="create-form__label--item">
+                    メールアドレス
+                </span>
+                <span class="create-form__label--required">
+                    ※
+                </span>
+            </div>
+            <div class="create-form__content">
+                <input type="email" name="email">
+            </div>
+        </div>
+        <div class="create-form__title">
+                <span class="create-form__label--item">
+                    住所
+                </span>
+                <span class="create-form__label--required">
+                    ※
+                </span>
+            </div>
+            <div class="create-form__content">
+                <input type="text" name="address">
+            </div>
+        </div>
+        <div class="create-form__title">
+                <span class="create-form__label--item">
+                    建物名
+                </span>
+            </div>
+            <div class="create-form__content">
+                <input type="text" name="building">
+            </div>
+            <div class="create-form__title">
+                <span class="create-form__label--item">
+                    お問い合わせの種類
+                </span>
+                <span class="create-form__label--required">
+                    ※
+                </span>
+            </div>
+            <div class="create-form__content">
+                <select name="select"  name="">
+                    <option value="選択してください">選択してください</option>
+                    <option value=""></option>
+                </select>
+            </div>
+            <div class="create-form__title">
+                <span class="create-form__label--item">
+                    お問い合わせの内容
+                </span>
+                <span class="create-form__label--required">
+                    ※
+                </span>
+            </div>
+            <div class="create-form__content">
+                <input type="text" name="email">
+            </div>
+            <div class="form__button">
+                <button class="form__button-submit" type="submit">
+                    確認画面
+                </button>
+            </div>
+            <input type="submit" value="">
+            確認画面
+        </div>
+
+        </div>
+
+
+
+
+            
+             
+
+    </form>
+</div>
